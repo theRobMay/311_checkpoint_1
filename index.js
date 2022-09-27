c5866d121a1055d25af364119bbe0212bdb79447
theRobMay <robbie.maysent@gmail.com>
2022-09-27 14:51:22 -0500

    adding a comment for static element on landing page

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 require('dotenv').config();
 const app = express();
 const port = process.env.MYSQL_PORT
-
+//setting up the static landing page
 app.get('/', (req, res) => res.send('<div><h2>How this works:</h2></div><div><ul><li>GET /users will provide a list of all users id, first and last name, and address in the database</li><li>GET /users/id will provide you with a specific user in the database, provided they exist</li><li>POST /users will allow you to add an entry that will be assigned an ID and returned to you so long as you provide the first name, last name, address, email, phone, username, user password</li><li>PUT /users/id will allow you to update a specific entrys field with new information specified in the body of your request</li><li>DELETE /users/id allows you to delete a specific entry from the database</li></ul></div>'))
 
 //middleware
